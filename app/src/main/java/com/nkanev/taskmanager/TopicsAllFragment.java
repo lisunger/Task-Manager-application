3cf55bf6cdabb3a7480b1b726c90109fefcced4d
Nikolay Kanev <>
2018-04-05 15:04:28 +0300

    added cardView to display the allTopicsFragment

--- app/src/main/java/com/nkanev/taskmanager/TopicsAllFragment.java
+++ app/src/main/java/com/nkanev/taskmanager/TopicsAllFragment.java
@@ -3,6 +3,8 @@ package com.nkanev.taskmanager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,14 +22,18 @@ public class TopicsAllFragment extends TopicsListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                inflater.getContext(),
-                android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.topics_all));
 
-        setListAdapter(adapter);
+        RecyclerView topicsRecycler =
+                (RecyclerView) inflater.inflate(R.layout.fragment_all_topics, container, false);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        String[] topicNames = new String[] {"eins", "zwei", "drei", "vier", "sechs"};
+        TopicsCardsAdapter adapter = new TopicsCardsAdapter(topicNames);
+        topicsRecycler.setAdapter(adapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        topicsRecycler.setLayoutManager(layoutManager);
+
+        return topicsRecycler;
     }
 
 }
--- app/src/main/java/com/nkanev/taskmanager/database/TasksSQLiteHelper.java
+++ app/src/main/java/com/nkanev/taskmanager/database/TasksSQLiteHelper.java
@@ -1,8 +1,12 @@
 package com.nkanev.taskmanager.database;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.Arrays;
 
 /**
  * Created by n.kanev on 03-Apr-18.
@@ -10,28 +14,42 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class TasksSQLiteHelper extends SQLiteOpenHelper {
 
-    private String TOPICS_TABLE_NAME = "TOPICS";
+    private static final String DB_NAME = "TASKMANAGER";
+    private static final String TABLE_TOPICS = "TOPICS";
+    private static final int DB_VERSION = 1;
 
     public TasksSQLiteHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+        super(context, DB_NAME, factory, DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        /* create table TOPICS with columns:
-         * _id: primary key
-         * name: text
-         */
+        /* create table TOPICS(
+            _id integer primary key autoincrement,
+            name text);
+        */
         StringBuilder sb = new StringBuilder();
         sb.append("create table ");
-        sb.append(TOPICS_TABLE_NAME);
-        sb.append("(_id integer primary key autoincrement, ");
-        sb.append("name text)");
+        sb.append(TABLE_TOPICS);
+        sb.append(" (_id integer primary key autoincrement, ");
+        sb.append("name text);");
 
         db.execSQL(sb.toString());
+
+        // add some predefined topics to the TOPICS table
+        insertTopic(db, "Topic1");
+        insertTopic(db, "Topic2");
+        insertTopic(db, "Topic3");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+
+    private static void insertTopic(SQLiteDatabase db, String name) {
+        ContentValues topic = new ContentValues();
+        topic.put("name", name);
+        db.insert(TABLE_TOPICS, null, topic);
     }
 }
