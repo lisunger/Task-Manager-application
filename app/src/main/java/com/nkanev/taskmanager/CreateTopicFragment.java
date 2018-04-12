d43cfbc01ca7a6d20b51a5c70602ba72d11a21e4
Nikolay Kanev <>
2018-04-12 14:47:41 +0300

    added dialog to enter a new topic and save it to the db

---
+++ app/src/main/java/com/nkanev/taskmanager/CreateTopicFragment.java
@@ -0,0 +1,85 @@
+package com.nkanev.taskmanager;
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateTopicFragment extends DialogFragment {
+
+    private CreateTopicDialogListener listener;
+    private EditText editTextNewTopic;
+
+    public CreateTopicFragment() {
+        // Required empty public constructor
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        builder.setTitle(R.string.new_topic_fragment_title);
+        View view = inflater.inflate(R.layout.fragment_new_topic, null);
+        editTextNewTopic = view.findViewById(R.id.editText_new_topic);
+        builder.setView(view);
+
+        builder.setPositiveButton(R.string.button_ok, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                listener.onDialogPositiveClick(editTextNewTopic.getText().toString());
+            }
+        });
+
+        builder.setNegativeButton(R.string.button_cancel, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //listener.onDialogNegativeClick(editTextNewTopic.getText().toString());
+            }
+        });
+
+        return builder.create();
+    }
+
+    /*
+        Not necessary, because onCreateDialog takes care of the layout
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_new_topic, container, false);
+        }
+    */
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            listener = (CreateTopicDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement CreateTopicDialogListener");
+        }
+    }
+
+    public interface CreateTopicDialogListener {
+        public void onDialogPositiveClick(String text);
+        //public void onDialogNegativeClick(String text);
+    }
+
+}
--- app/src/main/java/com/nkanev/taskmanager/MainActivity.java
+++ app/src/main/java/com/nkanev/taskmanager/MainActivity.java
@@ -1,12 +1,11 @@
 package com.nkanev.taskmanager;
 
-
-
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -15,15 +14,19 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.nkanev.taskmanager.database.TasksSQLiteHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CreateTopicFragment.CreateTopicDialogListener{
 
-    private static int index = 1;
+    private static final String TAG = "MainActivity";
+    private Toast toast;
+    private int pages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 new TopicsPagerAdapter(getSupportFragmentManager());
         ViewPager pager = findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
+        pages = pagerAdapter.getCount();
 
         TabLayout tabLayout = findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(pager);
@@ -53,40 +57,67 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_new_topic:
-
-                addTopicToDB(new Integer(index++).toString());
-                ViewPager pager = findViewById(R.id.pager);
-                Fragment fragment = getSupportFragmentManager().findFragmentByTag("android:switcher:" + R.id.pager + ":" + pager.getCurrentItem());
-                refreshFragment(fragment);
-
-                Toast t = Toast.makeText(this, "Add new topic", Toast.LENGTH_SHORT);
-                t.show();
-
+                CreateTopicFragment f = new CreateTopicFragment();
+                f.show(getSupportFragmentManager(), TAG);
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void addTopicToDB(String title) {
+    /**
+     * Add the title to the TOPICS table as a new topic
+     * @param title the name of the new topic
+     * @return <code>true</code> if the topic has been added successfully
+     */
+    private boolean addTopicToDB(String title) {
         SQLiteOpenHelper databaseHelper = new TasksSQLiteHelper(this);
-
+        long newId = -1;
         try {
             SQLiteDatabase db = databaseHelper.getWritableDatabase();
             ContentValues values = new ContentValues();
             values.put("name", title);
-            db.insert(TasksSQLiteHelper.TABLE_TOPICS, null, values);
+            newId = db.insert(TasksSQLiteHelper.TABLE_TOPICS, null, values);
         } catch (SQLiteException e) {
-            Toast t = Toast.makeText(this, "Database error", Toast.LENGTH_SHORT);
-            t.show();
+            showToast("Database error");
         } finally {
             databaseHelper.close();
         }
+        return newId != -1;
     }
 
-    private void refreshFragment(Fragment fragment) {
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.detach(fragment);
-        fragmentTransaction.attach(fragment);
-        fragmentTransaction.commit();
+    private void refreshPagerFragments() {
+        for(int i = 0; i < pages; i++) {
+            Fragment fragment = getSupportFragmentManager().findFragmentByTag("android:switcher:" + R.id.pager + ":" + 0);
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.detach(fragment);
+            fragmentTransaction.attach(fragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    private void showToast(String message) {
+        if(toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(this, message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @Override
+    public void onDialogPositiveClick(String text) {
+        Log.i(TAG, text);
+        // Create a new topic in the database with the given name
+        if(text != null && text.trim().length() > 0) {
+            boolean topicCreated = addTopicToDB(text);
+
+            if (topicCreated) {
+                showToast("Topic created");
+            }
+            else {
+                showToast("Error when creating new topic");
+            }
+
+            refreshPagerFragments();
+        }
     }
 
     private class TopicsPagerAdapter extends FragmentPagerAdapter {
